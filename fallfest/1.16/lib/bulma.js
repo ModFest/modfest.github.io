66ca1ab467cb4c620b82bcdcca0446b9139ae078
Prospector <prospectordev@gmail.com>
2020-09-24 13:46:13 -0700

    Start FallFest 1.16 site

---
+++ fallfest/1.16/lib/bulma.js
@@ -0,0 +1,9 @@
+// source: https://gist.github.com/Bradcomp/a9ef2ef322a8e8017443b626208999c1
+(function () {
+    const burger = document.querySelector('.burger');
+    const menu = document.querySelector('#' + burger.dataset.target);
+    burger.addEventListener('click', function () {
+        burger.classList.toggle('is-active');
+        menu.classList.toggle('is-active');
+    });
+})();
